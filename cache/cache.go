a5180a72ae08e559b6da9d3a68056fdaf74e0934
Mehdi <mehdi.mhiri@gmail.com>
2025-10-31 15:50:10 +0900

    fix(cache): use list and check partial prompt/suffix

--- cache/cache.go
+++ cache/cache.go
@@ -1,24 +1,29 @@
 package cache
 
 import (
+	"container/list"
 	"llm-language-server/lsp"
+	"strings"
 	"time"
 )
 
-type CacheValue struct {
-	Data []lsp.CompletionItem
-	Ex   time.Time
+type Cache struct {
+	Prompt string
+	Suffix string
+	Data   []lsp.CompletionItem
+	Ex     time.Time
 }
 
-var cacheMap map[string]CacheValue
+var cache *list.List
 var started = false
 
-var ex = 60 * time.Second
+var ex = 600 * time.Second
 
 func checkExpiredKeys() {
-	for key, value := range cacheMap {
+	for e := cache.Front(); e != nil; e = e.Next() {
+		value := e.Value.(Cache)
 		if time.Now().After(value.Ex) {
-			delete(cacheMap, key)
+			cache.Remove(e)
 		}
 	}
 }
@@ -28,7 +33,7 @@ func Set(prompt string, suffix string, value []lsp.CompletionItem) {
 		return
 	}
 
-	cacheMap[prompt+suffix] = CacheValue{Data: value, Ex: time.Now().Add(ex)}
+	cache.PushBack(Cache{Data: value, Prompt: prompt, Suffix: suffix, Ex: time.Now().Add(ex)})
 }
 
 func Get(prompt string, suffix string) ([]lsp.CompletionItem, bool) {
@@ -36,17 +41,28 @@ func Get(prompt string, suffix string) ([]lsp.CompletionItem, bool) {
 		return nil, false
 	}
 
-	value, exists := cacheMap[prompt+suffix]
-
-	if exists {
-		return value.Data, true
-	} else {
-		return nil, false
+	for e := cache.Front(); e != nil; {
+		next := e.Next()
+		value := e.Value.(Cache)
+		for _, item := range value.Data {
+			if strings.HasPrefix(value.Prompt+item.InsertText, prompt) && strings.HasSuffix(suffix, value.Suffix) {
+				clonedData := make([]lsp.CompletionItem, len(value.Data))
+				for i, item := range value.Data {
+					clonedItem := item
+					clonedItem.InsertText = item.InsertText[len(prompt)-len(value.Prompt):]
+					clonedData[i] = clonedItem
+				}
+				return clonedData, true
+			}
+		}
+		e = next
 	}
+
+	return []lsp.CompletionItem{}, false
 }
 
 func Reset() {
-	cacheMap = make(map[string]CacheValue)
+	cache = list.New()
 }
 
 var ticker *time.Ticker
